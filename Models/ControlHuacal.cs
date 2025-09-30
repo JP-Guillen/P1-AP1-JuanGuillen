8d963b08d59cfd64c63ad8fa4bb0ee1f50e11d50
JP-Guillen <juan_guillen@ucne.edu.do>
2025-09-29 21:10:16 -0400

    Creando modelo

--- Models/ControlHuacal.cs
+++ Models/ControlHuacal.cs
@@ -6,6 +6,14 @@ public class ControlHuacal
 
     [Key]
 
-    public int RegistroId { get; set; }
+    public int IdEntrada { get; set; }
+
+    DateTime fecha { get; set; } = DateTime.UtcNow;
+
+    [Required(ErrorMessage = "El nombre del cliente es obligatorio.")]
+    public string NombreCliente { get; set; }
+    public int cantidad { get; set; }
+    public double precio { get; set; }
+
 }
 

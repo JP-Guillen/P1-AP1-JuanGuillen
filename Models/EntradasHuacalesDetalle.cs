c8c59e0f4f2159608b8fe5a70b9cf3e69cd90c64
JP-Guillen <juan_guillen@ucne.edu.do>
2025-10-11 15:37:12 -0400

    Creando Modelos nuevos

---
+++ Models/EntradasHuacalesDetalle.cs
@@ -0,0 +1,24 @@
+﻿using P1_AP1_JuanGuillen.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class EntradasHuacalesDetalle
+{
+    [Key]
+    public int DetallesId { get; set; }
+    public int IdEntrada { get; set; }
+    public int TipoId { get; set; }
+    [Range(0, double.MaxValue, ErrorMessage = "La cantidad debe ser mayor que cero.")]
+    public int Cantidad { get; set; }
+
+    [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor que cero.")]
+    public double Precio { get; set; }
+
+    [ForeignKey("IdEntrada")]
+    [InverseProperty("EntradasHuacalesDetalles")]
+    public virtual EntradasHuacales EntradaHuacal { get; set; } = null;
+
+    [ForeignKey("TipoId")]
+    [InverseProperty("EntradasHuacalesDetalle")]
+    public virtual TiposHuacales TipoHuacal { get; set; }
+}
---
+++ Models/TiposHuacales.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P1_AP1_JuanGuillen.Models;
+
+public class TiposHuacales
+{
+        [Key]
+        public int TipoId { get; set; }
+
+        [Required(ErrorMessage = "Debe de tener una Descripcion Obligatoria")]
+        public string Descripcion { get; set; } = string.Empty;
+
+        [Range(0, int.MaxValue, ErrorMessage = "La Existencia debe ser Mayor o igual a 0")]
+        public int Existencia { get; set; }
+
+        [InverseProperty("TipoHuacal")]
+        public virtual ICollection<EntradasHuacalesDetalle> EntradasHuacalesDetalle { get; set; } = new List<EntradasHuacalesDetalle>();
+}
